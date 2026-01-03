16148bd793c20da0e6f100e074f20be22f392a01
Aaliyahussain27 <aaliyahussain2709@gmail.com>
2026-01-04 03:11:45 +0530

    navigation issue resolved

--- app/src/main/java/com/example/expensetracker/navigation/Navgraph.kt
+++ app/src/main/java/com/example/expensetracker/navigation/Navgraph.kt
@@ -1,18 +1,24 @@
 package com.example.expensetracker.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.expensetracker.screens.add.AddExpenseScreen
 import com.example.expensetracker.screens.home.HomeScreen
+import com.example.expensetracker.screens.home.HomeViewModel
 
 @Composable
 fun NavGraph(navController: NavHostController) {
     NavHost(navController = navController, startDestination = "home") {
 
         composable("home") {
-            HomeScreen(onNavigate = {
+            val homeViewModel: HomeViewModel = viewModel()
+
+            HomeScreen(
+                viewModel = homeViewModel,
+                onNavigate = {
                 navController.navigate("add_expense")
             })
         }
